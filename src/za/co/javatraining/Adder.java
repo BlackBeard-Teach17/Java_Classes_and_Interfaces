9c7187b85340d15777484ebc696e377bab781506
Blackbeard-Teach <keamogetswemfoloe@gmail.com>
2021-03-09 16:06:00 +0200

    Added interface to make program more modular

--- src/za/co/javatraining/Adder.java
+++ src/za/co/javatraining/Adder.java
@@ -1,6 +1,6 @@
 package za.co.javatraining;
 
-public class Adder extends CalculateBase{
+public class Adder extends CalculateBase implements MathProcessing{
     public  Adder(){}
     public Adder(double leftVal, double rightVal) {
         super(leftVal, rightVal);
@@ -11,5 +11,18 @@ public class Adder extends CalculateBase{
     {
         double value = getLeftVal() + getRightVal();
         setResult(value);
+    }
+
+    @Override
+    public String getKeyWord() {
+        return "add";
+    }
+
+    @Override
+    public double doCalculation(double leftVal, double rightVal) {
+        setLeftVal(leftVal);
+        setRightVal(rightVal);
+        calculate();
+        return getResult();
     }
 }
---
+++ src/za/co/javatraining/MathProcessing.java
@@ -0,0 +1,7 @@
+package za.co.javatraining;
+
+public interface MathProcessing {
+    String SEPARATOR = " ";
+    String getKeyWord();
+    double doCalculation(double leftVal, double rightVal);
+}
