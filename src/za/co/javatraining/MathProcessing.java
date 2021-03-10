a3522b472bc5ac343df9c8531d7ecd6bc61e0b23
Blackbeard-Teach <keamogetswemfoloe@gmail.com>
2021-03-10 16:23:53 +0200

    Added formatted output and power of functionality

--- src/za/co/javatraining/MathProcessing.java
+++ src/za/co/javatraining/MathProcessing.java
@@ -4,4 +4,8 @@ public interface MathProcessing {
     String SEPARATOR = " ";
     String getKeyWord();
     double doCalculation(double leftVal, double rightVal);
+    default String getFormattedOutput()
+    {
+        return null;
+    }
 }
---
+++ src/za/co/javatraining/PowerOf.java
@@ -0,0 +1,21 @@
+package za.co.javatraining;
+
+public class PowerOf implements MathProcessing{
+    private String formattedOutput;
+    @Override
+    public String getKeyWord() {
+        return "power";
+    }
+
+    @Override
+    public double doCalculation(double leftVal, double rightVal) {
+        double product = Math.pow(leftVal, rightVal);
+        formattedOutput = leftVal + "to the power of " + rightVal + " is " + product;
+        return product;
+    }
+
+    @Override
+    public String getFormattedOutput() {
+        return formattedOutput;
+    }
+}
